cdfd1137446ca0eaa13f730ede465268c2d9039e
David Baker <david_baker@iinet.net.au>
2019-04-06 18:47:18 +0100

    Implement most of the System Detail tags (some don't seem possible) and unti tests to ensure they at least complete without errors.

--- CumulusMX/TagDetails/SystemDetails.cs
+++ CumulusMX/TagDetails/SystemDetails.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Globalization;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using CumulusMX.Extensions;
+using UnitsNet;
 
 namespace CumulusMX.TagDetails
 {
@@ -7,24 +13,25 @@ namespace CumulusMX.TagDetails
     {
         public string RegistrationName => "System";
 
-        public string OsVersion => throw new NotImplementedException();
-        public string OsLanguage => throw new NotImplementedException();
-        public string SystemUpTime => throw new NotImplementedException();
-        public string ProgramUpTime => throw new NotImplementedException();
-        public string CpuName => throw new NotImplementedException();
-        public string CpuCount => throw new NotImplementedException();
-        public string MemoryStatus => throw new NotImplementedException();
-        public string DisplayMode => throw new NotImplementedException();
-        public string AllocatedMemory => throw new NotImplementedException();
-        public string DiskSize => throw new NotImplementedException();
-        public string DiskFree => throw new NotImplementedException();
+        public string OsType => Enum.GetName(typeof(PlatformID), Environment.OSVersion.Platform);
+        public string OsVersion => Environment.OSVersion.Version.ToString();
+        public string OperatingSystem => RuntimeInformation.OSDescription;
+        public string OsLanguage => CultureInfo.CurrentCulture.DisplayName;
+        //public TimeSpan SystemUpTime => throw new NotImplementedException(); // Don't think this is possible cross platform
+        public TimeSpan ProgramUpTime => DateTime.Now - Process.GetCurrentProcess().StartTime;
+        public string OsPlatform => Enum.GetName(typeof(Architecture), RuntimeInformation.OSArchitecture);
+        public int CpuCount => Environment.ProcessorCount;
+        //public string MemoryStatus =>  // Don't think this is possible cross platform
+        //public string DisplayMode => throw new NotImplementedException(); // Don't think this is possible cross platform
+        public Information AllocatedMemory => Information.FromBytes(Environment.WorkingSet);
+        public Information DiskSize => Information.FromBytes(new DriveInfo(new FileInfo(Assembly.GetEntryAssembly().FullName).Directory.Root.FullName).TotalSize);
+        public Information DiskFree => Information.FromBytes(new DriveInfo(new FileInfo(Assembly.GetEntryAssembly().FullName).Directory.Root.FullName).AvailableFreeSpace);
         public string LatestError => throw new NotImplementedException();
-        public string LatestErrorDate => throw new NotImplementedException();
-        public string LatestErrorTime => throw new NotImplementedException();
-        public string ErrorLight => throw new NotImplementedException();
-        public string Version => throw new NotImplementedException();
-        public string Build => throw new NotImplementedException();
-        public string Update => throw new NotImplementedException();
-        
+        public DateTime LatestErrorDate => throw new NotImplementedException();
+        public bool ErrorLight => throw new NotImplementedException();
+        public string Version => Assembly.GetEntryAssembly().GetName().Version.ToString();
+        public string Build => Assembly.GetEntryAssembly().GetName().Version.Build.ToString();
+        public DateTime BuildDate => new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+
     }
 }
