f3e2dc6d7896701a9c6edc73f2583f1684a70bf5
David Baker <david_baker@iinet.net.au>
2019-04-13 09:49:32 +0100

    Changed string template implementatin to use square brackets as delimiters.

---
+++ CumulusMX.Common/StringTemplate/DefaultDateRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Antlr4.StringTemplate;
+using CultureInfo = System.Globalization.CultureInfo;
+
+namespace CumulusMX.Common.StringTemplate
+{
+        /** Based on StringTemplate4 NumberRendered class, this takes any valid format string for string.Format
+         *  for formatting. Also takes a default value on construction - which will be used if no format parameter
+         *  is specified on the individual template entries.
+         */
+
+        public class DefaultDateRenderer : IAttributeRenderer
+        {
+            private readonly string _defaultFormat;
+
+            public DefaultDateRenderer(string defaultFormat)
+            {
+                _defaultFormat = defaultFormat;
+            }
+
+            public virtual string ToString(object o, string formatString, CultureInfo culture)
+            {
+                // o will be instance of Number
+                if (formatString == null)
+                    return string.Format(culture, _defaultFormat, o);
+
+            return string.Format(culture, formatString, o);
+            }
+        }
+    }
+
