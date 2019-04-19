6d0da7f7a6dba23962678500f762bfa3c27939f9
David Baker <david_baker@iinet.net.au>
2019-04-19 09:07:00 +0100

    Core functionality to do FTP reporter

---
+++ Extensions/FtpDataReporter/FtpDataReporterSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+using CumulusMX.Extensions;
+using CumulusMX.Extensions.DataReporter;
+
+namespace FtpDataReporter
+{
+    public class FtpDataReporterSettings : DataReporterSettingsGeneric
+    {
+        public FtpDataReporterSettings(IConfigurationProvider settings) : base(settings)
+        {
+            SectionName = "FtpDataReporter";
+        }
+
+        public string User { get; set; }
+        public SecureString Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+        public bool RenameFiles { get; set; }
+        public bool ActiveFtp { get; set; }
+        public bool DisableEpsv { get; set; }
+        public string FtpDirectory { get; set; }
+        public bool IncludeGraphDataFiles { get; set; }
+        public bool RealtimeEnabled { get; set; }
+        public bool IncludeStandardFiles { get; set; }
+        public bool DeleteBeforeUpload { get; set; }
+        public bool RealtimeGauges { get; set; }
+        public bool RealtimeText { get; set; }
+        public string NoaaDirectory { get; set; }
+        public List<UploadFileDetails> ExtraFiles { get; set; }
+        public bool SaveLocal { get; internal set; }
+        public string LocalSavePath { get; internal set; }
+        public string ExternalProgram { get; internal set; }
+        public string ExternalParams { get; internal set; }
+        public string RealtimeParams { get; internal set; }
+        public string RealtimeProgram { get; internal set; }
+        public int RealtimeInterval { get; internal set; }
+    }
+}
---
+++ Extensions/FtpDataReporter/UploadFileDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FtpDataReporter
+{
+    public struct UploadFileDetails
+    {
+        public string local;
+        public string remote;
+        public bool process;
+        public bool binary;
+        public bool realtime;
+        public bool FTP;
+        public bool UTF8;
+    }
+}
