286c629ccc24e4274a89941d340f2a77b304dd75
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2021-02-16 22:53:51 +0000

    b3112 - fix gw1000 auto discovery

--- CumulusMX/Properties/AssemblyInfo.cs
+++ CumulusMX/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cumulus MX")]
-[assembly: AssemblyDescription("Build 3111")]
+[assembly: AssemblyDescription("Build 3112")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cumulus MX")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.0.3111")]
-[assembly: AssemblyFileVersion("3.10.0.3111")]
+[assembly: AssemblyVersion("3.10.0.3112")]
+[assembly: AssemblyFileVersion("3.10.0.3112")]
