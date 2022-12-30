448209207b89a5b4d12c539dc8a11637457d5c30
Mark Crossley <public@wilsmlowastro.com>
2022-12-30 11:51:54 +0000

    Version uplift

--- CumulusMX/Properties/AssemblyInfo.cs
+++ CumulusMX/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cumulus MX")]
-[assembly: AssemblyDescription("Version 3.23.0 - Build 3219")]
+[assembly: AssemblyDescription("Version 3.23.0 - Build 3220")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cumulus MX")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.23.0.3219")]
-[assembly: AssemblyFileVersion("3.23.0.3219")]
+[assembly: AssemblyVersion("3.23.0.3220")]
+[assembly: AssemblyFileVersion("3.23.0.3220")]
