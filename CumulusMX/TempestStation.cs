8cb4e2140e8d596a2271c5852e3866037a8fb041
Mark Crossley <public@wilsmlowastro.com>
2023-04-26 10:04:35 +0100

    Tempest station now forces MX to calculate the average wind speed

--- CumulusMX/TempestStation.cs
+++ CumulusMX/TempestStation.cs
@@ -28,6 +28,9 @@ namespace CumulusMX
 			// Tempest does not provide wind chill
 			cumulus.StationOptions.CalculatedWC = true;
 
+			// Tempest does not provide average wind speeds
+			cumulus.StationOptions.UseWind10MinAvg = true;
+
 			LoadLastHoursFromDataLogs(cumulus.LastUpdateTime);
 
 			Task.Run(getAndProcessHistoryData);// grab old data, then start the station
