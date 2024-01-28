fb3b3174973a00f65622cc138d9ef3eb94781565
Mark Crossley <public@wilsmlowastro.com>
2024-01-28 22:20:05 +0000

    Add decode of Ecowitt Heap size for devices using the GW1000 API

--- CumulusMX/GW1000Station.cs
+++ CumulusMX/GW1000Station.cs
@@ -1186,6 +1186,10 @@ namespace CumulusMX
 								}
 								idx += 8;
 								break;
+							case 0x6C: // Heap size - has constant offset of +3692 to HTTP value????
+								StationFreeMemory = (int) GW1000Api.ConvertBigEndianUInt32(data, idx);
+								idx += 4;
+								break;
 							case 0x70: // WH45 CO₂
 								batteryLow = batteryLow || DoCO2Decode(data, idx);
 								idx += 16;
