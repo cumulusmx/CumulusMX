1aef0c4cd5a407d93a0864e2a366b536e1e8601b
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2025-09-30 12:25:20 +0100

    Enable Ecowitt SD card file reading retries

--- CumulusMX/Stations/EcowittLocalApi.cs
+++ CumulusMX/Stations/EcowittLocalApi.cs
@@ -1021,7 +1021,7 @@ namespace CumulusMX.Stations
 
 					cumulus.LogDebugMessage($"LocalApi.GetSdFileContents: Extracting all lines from starting time {startTime.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture)}");
 
-					while ((line = await streamReader.ReadLineAsync()) != null)
+					while ((line = await streamReader.ReadLineAsync(token)) != null)
 					{
 						count++;
 
@@ -1043,8 +1043,13 @@ namespace CumulusMX.Stations
 							if (fields.Length < 10)
 							{
 								cumulus.LogWarningMessage($"LocalApi.GetSdFileContents: File {fileName} header line is malformed");
-								cumulus.LogDataMessage("Header line = " + line);
-								return null;
+								cumulus.LogMessage("Header line = " + line);
+								// try again?
+								if (retries >= 0)
+								{
+									cumulus.LogMessage("LocalApi.GetSdFileContents: Try and fetch the file again");
+								}
+								break;
 							}
 
 							useTimeStamp = fields[1].ToLower() == "timestamp";
@@ -1089,7 +1094,10 @@ namespace CumulusMX.Stations
 
 					cumulus.LogDebugMessage($"LocalApi.GetSdFileContents: Extracted {result.Count} lines from {fileName}");
 
-					return result;
+					if (result.Count > 0)
+					{
+						return result;
+					}
 				}
 				catch (HttpRequestException ex)
 				{
